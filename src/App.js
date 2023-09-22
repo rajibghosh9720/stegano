f9f57ca613eb951238bd1a5bc83b30023525a3a3
wasimreja <iwasimreja@gmail.com>
2023-09-22 20:04:14 +0530

    Fix navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import './App.css';
+import "./App.css";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-import React from 'react'
+import React from "react";
 import ImageCrypt from "./pages/ImageCrypt";
 import ImageDecrypt from "./pages/ImageDecrypt";
 import About from "./pages/About";
@@ -15,36 +10,28 @@ import About from "./pages/About";
 function App() {
   return (
     <Router>
-
       <nav className="navbar">
+        <a href="/" className="BrandName">
+          Stegano
+        </a>
         <ul className="nav">
-          <a href="/" className="BrandName">Stegano</a>
           <li>
-            <Link to="/stegano/">Encode</Link>
+            <Link to="/">Encode</Link>
           </li>
           <li>
-            <Link to="/stegano/decode">Decode</Link>
+            <Link to="/decode">Decode</Link>
           </li>
           <li>
-            <Link to="/stegano/about">About</Link>
+            <Link to="/about">About</Link>
           </li>
         </ul>
       </nav>
 
-      <Switch>
-        <Route path="/stegano/about">
-          <About />
-        </Route>
-        <Route path="/stegano/decode">
-          <ImageDecrypt />
-        </Route>
-        <Route path="/stegano/">
-          <ImageCrypt />
-        </Route>
-        <Route path="/">
-          <ImageCrypt />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/" element={<ImageCrypt />} />
+        <Route path="/decode" element={<ImageDecrypt />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </Router>
   );
 }
